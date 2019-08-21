65b782aadfd9513cb8e2317d57a34336f9b3d0c8
vishnupriya-58 <54269367+vishnupriya-58@users.noreply.github.com>
2019-08-21 14:45:10 +0530

    Update Runner_class.java

--- src/test/java/mystepdefinition/Runner_class.java
+++ src/test/java/mystepdefinition/Runner_class.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="Feature456",glue="mystepdefinition",plugin={"json:target/cucmber.json"})
+@CucumberOptions(plugin={"json:target/cucmber.json"})
 
 public class Runner_class {
 	
